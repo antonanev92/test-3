6f660eb40e3707fea9ed3bdfab20da6b478b5a18
antonanev92 <anton.anev92@gmail.com>
2021-03-18 16:20:02 +0300

    delete 2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(morgan('tiny'))
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4326
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
