05af35ea29ef94326bc595582d79b9e035156c21
antonanev92 <anton.anev92@gmail.com>
2021-03-18 16:24:46 +0300

    delete 3

--- index.js
+++ index.js
@@ -31,3 +31,15 @@ app.get('/:name', (req, res, next) => {
     }
 
 })
+
+
+const monsterStoreInventory = { fenrirs: 4, banshees: 1, jerseyDevils: 4, krakens: 3 };
+app.get('/monsters-inventory/:name', (req, res, next) => {
+  const monsterInventory = monsterStoreInventory[req.params.name];
+  if (monsterInventory) {
+    res.send(monsterInventory);
+  } else {
+    res.status(404).send('Monster not found');
+  }
+});
+
